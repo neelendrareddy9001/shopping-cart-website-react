192294a91d0f904c6af6086cfc0788fc4bf5b798
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-01-18 23:04:43 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Cart './Components/Cart';
 
 function App() {
   return (
     <div className="App">
-      
+        <Cart />
     </div>
   );
 }
