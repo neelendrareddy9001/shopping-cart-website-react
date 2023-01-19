660411d4534df48ab65de6529393e35324ad6130
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-01-19 16:54:27 +0100

    Update Cart.js

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -4,14 +4,22 @@ import { products } from './Products';
 import ContextCart from './ContextCart';
 
 export const CartContext = createContext();
+ 
+const initialState = {
+    item : products,
+    totalAmount : 0,
+    totalItem : 0,
+};
 
 const Cart = () => {
      // const [item, setItem] = useState(products);
+     const [state, dispatch] = useReducer(reducer, initialState);
     
-    const [item, setItem] = useState(products);
+     // const [item, setItem] = useState(products);
     return (
         <>
-           <CartContext.Provider value={products}>
+           <CartContext.Provider value={{...state}}>
+         
            <ContextCart />
         </CartContext.Provider> 
         </>
